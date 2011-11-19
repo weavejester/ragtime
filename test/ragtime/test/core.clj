1f6e47c92eeb131a142f29115d83bda3c826b444
James Reeves <jreeves@weavejester.com>
2011-11-19 23:01:39 +0000

    Added some tests for migration macro

--- test/ragtime/test/core.clj
+++ test/ragtime/test/core.clj
@@ -1,6 +1,18 @@
 (ns ragtime.test.core
-  (:use [ragtime.core])
-  (:use [clojure.test]))
+  (:use [ragtime.core]
+        [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-migration
+  (testing "migration id"
+    (is (= (id (migration "foo"))
+           "foo")))
+  (let [s (atom {})
+        m (migration "foo"
+            (:up (swap! s assoc :x 1))
+            (:down (swap! s dissoc :x)))]
+    (testing "migration up"      
+      (up m)
+      (is (= @s {:x 1})))
+    (testing "migration down"
+      (down m)
+      (is (= @s {})))))
