9a4d9d27e7bd0a09b5818057bd9260cac3824c00
James Reeves <jreeves@weavejester.com>
2011-11-30 21:12:10 +0000

    Removed Migration protocol; using maps instead

--- src/ragtime/core.clj
+++ src/ragtime/core.clj
@@ -5,50 +5,25 @@
         ragtime.database)
   (:require [ragtime.strategy :as strategy]))
 
-(defprotocol Migration
-  "The migration protocol is meant to be reified into a single migration
-  unit. See the defmigration macro."
-  (id [migration] "A unique identifier for the migration")
-  (up [migration] "Apply the migration to the database")
-  (down [migration] "Rollback the migration"))
-
-(defmacro migration
-  "Creates a new migration with the supplied id and clauses. The id must be
-  unique. The clauses consist of an :up form, which is evaluated when the
-  migration is applied, and a :down form, which is evalidated when the migration
-  is rolled back.
-
-  (migration \"a-unique-id\"
-     (:up (apply-migration))
-     (:down (rollback-migration))"
-  [id & clauses]
-  (let [methods (reduce
-                 (fn [m [k & body]] (assoc m k body))
-                 {}
-                 clauses)]
-    `(reify Migration
-       (id [_] (name ~id))
-       (up [_] ~@(:up methods))
-       (down [_] ~@(:down methods)))))
-
 (defonce defined-migrations (atom {}))
 
 (defn remember-migration [migration]
-  (swap! defined-migrations assoc (id migration) migration))
+  (swap! defined-migrations assoc (:id migration) migration))
 
 (defmacro defmigration
-  "Defines a migration in the current namespace. See ragtime.core/migration.
+  "Defines a migration in the current namespace. The name of the migration must
+  be unique. The migration itself is a map consisting of a :up function for
+  applying the migration, and a :down function to rollback the migration.
 
-  (defmigration a-migration
-    (:up (apply-migration))
-    (:down (rollback-migration)))"
-  [name & args]
-  (let [[name args] (name-with-attributes name args)]
-    `(let [id# (str (ns-name *ns*) "/" ~(str name))]
-       (def ~name (migration id# ~@args))
-       (defonce ~'db-migrations (atom []))
-       (swap! ~'db-migrations conj ~name)
-       (remember-migration ~name))))
+  (defmigration some-migration
+    {:up   #(apply-migration db)
+     :down #(rollback-migration db)})"
+  [name & [migration]]
+  `(let [id# (str (ns-name *ns*) "/" ~(str name))]
+     (def ~name (assoc ~migration :id id#))
+     (defonce ~'db-migrations (atom []))
+     (swap! ~'db-migrations conj ~name)
+     (remember-migration ~name)))
 
 (defn- namespace-name [ns]
   (if (instance? clojure.lang.Namespace ns)
@@ -85,14 +60,14 @@
   "Apply a single migration to a database."
   [db migration]
   (remember-migration migration)
-  (up migration)
-  (add-migration-id db (id migration)))
+  ((:up migration))
+  (add-migration-id db (:id migration)))
 
 (defn rollback
   "Rollback a migration already applied to the database."
   [db migration]
-  (down migration)
-  (remove-migration-id db (id migration)))
+  ((:down migration))
+  (remove-migration-id db (:id migration)))
 
 (defn migrate-all
   "Migrate all migrations using the supplied strategy. The strategy defines
--- test/ragtime/test/core.clj
+++ test/ragtime/test/core.clj
@@ -4,32 +4,23 @@
         ragtime.database)
   (:require [ragtime.strategy :as strategy]))
 
-(deftest test-migration
-  (testing "migration id"
-    (is (= (id (migration "foo"))
-           "foo")))
-  (let [s (atom {})
-        m (migration "foo"
-            (:up (swap! s assoc :x 1))
-            (:down (swap! s dissoc :x)))]
-    (testing "migration up"      
-      (up m)
-      (is (= @s {:x 1})))
-    (testing "migration down"
-      (down m)
-      (is (= @s {})))))
-
 (defmigration test-migrate-1)
-(defmigration test-migrate-2)
+
+(defmigration test-migrate-2
+  {:up   #(println "up")
+   :down #(println "down")})
 
 (deftest test-defmigration
   (testing "id based on symbol"
-    (is (= (id test-migrate-1)
+    (is (= (:id test-migrate-1)
            "ragtime.test.core/test-migrate-1")))
   (testing "migrations listed in order"
     (is (= (list-migrations 'ragtime.test.core)
            [test-migrate-1
-            test-migrate-2]))))
+            test-migrate-2])))
+  (testing "retains up and down keys"
+    (is (:up test-migrate-2))
+    (is (:down test-migrate-2))))
 
 (deftype InMemoryDB [migrations]
   Database
@@ -46,9 +37,9 @@
 (deftest test-migrate-and-rollback
   (let [a  (atom {})
         db (in-memory-db)
-        m  (migration "m"
-             (:up (swap! a assoc :x 1))
-             (:down (swap! a dissoc :x)))]
+        m  {:id "m"
+            :up #(swap! a assoc :x 1)
+            :down #(swap! a dissoc :x)}]
     (testing "migrate"
       (migrate db m)
       (is (= @a {:x 1}))
@@ -61,15 +52,15 @@
 (deftest test-migrate-all
   (let [store   (atom {})
         db      (in-memory-db)
-        assoc-x (migration "assoc-x"
-                  (:up (swap! store assoc :x 1))
-                  (:down (swap! store dissoc :x)))
-        assoc-y (migration "assoc-y"
-                  (:up (swap! store assoc :y 2))
-                  (:down (swap! store dissoc :y)))
-        assoc-z (migration "assoc-z"
-                  (:up (swap! store assoc :z 3))
-                  (:down (swap! store dissoc :z)))]
+        assoc-x {:id "assoc-x"
+                 :up #(swap! store assoc :x 1)
+                 :down #(swap! store dissoc :x)}
+        assoc-y {:id "assoc-y"
+                 :up #(swap! store assoc :y 2)
+                 :down #(swap! store dissoc :y)}
+        assoc-z {:id "assoc-z"
+                 :up #(swap! store assoc :z 3)
+                 :down #(swap! store dissoc :z)}]
     (migrate-all db [assoc-x assoc-y])
     (is (= @store {:x 1 :y 2}))
     (migrate-all db [assoc-x assoc-z] strategy/rebase)
