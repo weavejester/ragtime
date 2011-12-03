50f50ce6ca00919083c94cbd8e5aae4e77e325a9
James Reeves <jreeves@weavejester.com>
2011-12-03 21:20:52 +0000

    Removed tools.macro and core.incubator deps

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject ragtime "1.0.0-SNAPSHOT"
   :description "A database-independent migration library"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/tools.macro "0.1.0"]
-                 [org.clojure/core.incubator "0.1.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]])
