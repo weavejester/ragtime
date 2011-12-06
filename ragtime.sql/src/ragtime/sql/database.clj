ba4df5b63256640e3e7be44ff6266ba4fc1f314b
Phil Hagelberg <technomancy@gmail.com>
2011-12-05 18:01:53 -0800

    Read migrations from disk, call migrate-all.

--- ragtime.sql/src/ragtime/sql/database.clj
+++ ragtime.sql/src/ragtime/sql/database.clj
@@ -1,12 +1,14 @@
 (ns ragtime.sql.database
-  (:use [ragtime.core :only (Migratable)])
+  (:use [ragtime.core :only [Migratable migrate-all]])
   (:require [clojure.java.jdbc :as sql]
-            [clojure.java.io :as io])
-  (:import (java.util Date)))
+            [clojure.java.io :as io]
+            [clojure.string :as str])
+  (:import (java.util Date)
+           (java.net URI)))
 
-(def ^:private migrations-table "ragtime_migrations")
+(def ^{:private true} migrations-table "ragtime_migrations")
 
-(defn ^:internal ensure-migrations-table-exists [db]
+(defn- ensure-migrations-table-exists [db]
   ;; TODO: is there a portable way to detect table existence?
   (sql/with-connection db
     (try
@@ -15,7 +17,7 @@
                         [:created_at "datetime"])
       (catch Exception _))))
 
-(defrecord SqlDatabase [classname subprotocol subname user password]
+(defrecord SqlDatabase [host port path scheme user password subname classname]
   Migratable
   (add-migration-id [db id]
     (sql/with-connection db
@@ -34,3 +36,52 @@
       (sql/with-query-results results
         ["SELECT id FROM ragtime_migrations ORDER BY created_at"]
         (vec (map :id results))))))
+
+(defn make-database [map]
+  ;; wait, srsly? this is how you do it?
+  (into (SqlDatabase. nil nil nil nil nil nil nil nil) map))
+
+(defn database-from-uri [uri]
+  (if (instance? URI uri)
+    ;; TODO: should this have been public?
+    (make-database (#'clojure.java.jdbc.internal/parse-properties-uri uri))
+    (database-from-uri (URI. uri))))
+
+(def ^{:private true} migration-pattern #"(.*)\.(up|down)\.sql$")
+
+(defn- migration? [file]
+  (re-find migration-pattern (.getName (io/file file))))
+
+(defn- migration-id [file]
+  (second (re-find migration-pattern (.getName (io/file file)))))
+
+(defn- get-migration-files [dir]
+  (->> (.listFiles (io/file dir))
+       (filter migration?)
+       (sort)
+       (group-by migration-id)))
+
+(defn- run-sql-fn [file]
+  (fn [db]
+    (sql/with-connection db
+      (sql/transaction
+       (sql/do-commands (slurp file))))))
+
+(defn- make-migration [[id [down up]]]
+  {:id id
+   :order (or (second (re-find #"^(\d+)" id)) id)
+   :up (run-sql-fn up)
+   :down (run-sql-fn down)})
+
+(defn migrations [dir]
+  (map make-migration (get-migration-files dir)))
+
+;; TODO: this might not be the best way of invoking, but it works
+;; nicely with lein run. Migrating shouldn't necessarily be a lein
+;; plugin since it needs to be easy to make happen in production, not
+;; just during development. So lein run may fit the bill?
+(defn -main
+  ([dir db] (migrate-all db (migrations dir)))
+  ([dir] (-main dir (database-from-uri (System/getenv "DATABASE_URL"))))
+  ;; TODO: can we make this work with files in jars?
+  ([] (-main "migrations")))
--- ragtime.sql/test/ragtime/sql/test/database.clj
+++ ragtime.sql/test/ragtime/sql/test/database.clj
@@ -5,8 +5,9 @@
                              applied-migration-ids]])
   (:require [clojure.java.jdbc :as sql]))
 
-(def test-db (ragtime.sql.database.SqlDatabase.
-              "org.h2.Driver" "h2" "mem:test_db" "test" ""))
+(def test-db (make-database {:classname "org.h2.Driver"
+                             :subprotocol "h2"
+                             :subname "mem:test_db"}))
 
 (defn h2-fixture [f]
   (sql/with-connection test-db
@@ -21,3 +22,22 @@
   (is (= ["12" "13" "20"] (applied-migration-ids test-db)))
   (remove-migration-id test-db "13")
   (is (= ["12" "20"] (applied-migration-ids test-db))))
+
+(deftest test-find-migrations
+  (let [migrations-found (migrations "test-resources/migrations")]
+    (is (every? fn? (map :up migrations-found)))
+    (is (every? fn? (map :down migrations-found)))
+    (is (= [{:id "20111202110600-create-foo-table", :order "20111202110600"}
+            {:id "20111202113000-create-bar-table", :order "20111202113000"}]
+           (for [m migrations-found]
+             (dissoc m :up :down))))))
+
+(deftest test-running-migrations
+  (sql/with-connection test-db
+    (-main "test-resources/migrations" test-db)
+    (sql/insert-values :foo [:id] [1000] [2000])
+    (sql/insert-values :bar [:id] [1000] [2000])
+    (is (= [1000 2000] (sql/with-query-results results ["select * from foo"]
+                         (vec (sort (map :id results))))))
+    (is (= [1000 2000] (sql/with-query-results results ["select * from bar"]
+                         (vec (sort (map :id results))))))))
