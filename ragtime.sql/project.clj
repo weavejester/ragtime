a8f9cff11061bf1e2ebf6aa172e597cc8c47699a
Fred Daoud <xf2697@fastmail.fm>
2012-05-07 22:45:56 -0400

    As suggested by weavejester, added remember-migrations to migrate-all. Also changed the versions to 0.2.1-SNAPSHOT, for ongoing development until publishing the next version.

--- ragtime.sql/project.clj
+++ ragtime.sql/project.clj
@@ -1,6 +1,6 @@
-(defproject ragtime/ragtime.sql "0.2.0"
+(defproject ragtime/ragtime.sql "0.2.1-SNAPSHOT"
   :description "Ragtime migrations for SQL databases"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [ragtime/ragtime.core "0.2.0"]
+                 [ragtime/ragtime.core "0.2.1-SNAPSHOT"]
                  [org.clojure/java.jdbc "0.1.1"]]
   :dev-dependencies [[com.h2database/h2 "1.3.160"]])
