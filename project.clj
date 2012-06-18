5d581450b511ef36456f2696efdd209d8732fdfe
James Reeves <jreeves@weavejester.com>
2012-06-18 22:01:56 +0100

    Released 0.2.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ragtime "0.2.0"
+(defproject ragtime "0.2.1"
   :description "A database-independent migration library"
-  :dependencies [[ragtime/ragtime.core "0.2.0"]
-                 [ragtime/ragtime.sql "0.2.0"]]
+  :dependencies [[ragtime/ragtime.core "0.2.1"]
+                 [ragtime/ragtime.sql "0.2.1"]]
   :dev-dependencies [[lein-sub "0.1.1"]]
   :sub ["ragtime.core" "ragtime.sql"])
