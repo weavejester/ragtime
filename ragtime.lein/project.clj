eef66e81b1f2ba7c47eb9a3368b11809d2ea9298
James Reeves <jreeves@weavejester.com>
2012-08-27 00:38:59 +0100

    Added 'lein migrate' commmand to plugin

--- ragtime.lein/project.clj
+++ ragtime.lein/project.clj
@@ -1,3 +1,4 @@
-(defproject ragtime.lein "0.3.0-SNAPSHOT"
+(defproject ragtime/ragtime.lein "0.3.0-SNAPSHOT"
   :description "A leiningen plugin for Ragtime."
+  :dependencies [[leinjacker "0.2.0"]]
   :eval-in-leiningen true)
--- ragtime.lein/src/leiningen/migrate.clj
+++ ragtime.lein/src/leiningen/migrate.clj
@@ -1,4 +1,30 @@
-(ns leiningen.migrate)
+(ns leiningen.migrate
+  (:use [leinjacker.eval :only (eval-in-project)])
+  (:require [leinjacker.deps :as deps]))
 
-(defn migrate [project & args]
-  (println "Do nothing"))
+(defn load-namespaces
+  "Create require forms for each of the supplied symbols. This exists because
+  Clojure cannot load and use a new namespace in the same eval form."
+  [& syms]
+  `(require
+    ~@(for [s syms :when s]
+        `'~(if-let [ns (namespace s)]
+             (symbol ns)
+             s))))
+
+(defn migrate
+  "Migrate the project database to the latest version."
+  [project & args]
+  (let [migrations (-> project :ragtime :migrations)
+        database   (-> project :ragtime :database)]
+    (eval-in-project
+     (-> project
+         (deps/add-if-missing '[ragtime/ragtime.core "0.3.0-SNAPSHOT"])
+         (deps/add-if-missing '[ragtime/ragtime.sql "0.3.0-SNAPSHOT"]))
+     `(ragtime.core/migrate-all
+       (ragtime.core/connection ~database)
+       (~migrations))
+     (load-namespaces
+      'ragtime.core
+      'ragtime.sql.database
+      migrations))))
