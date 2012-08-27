cfaad5426a8afb332ab24a5a4d0ce34f87d50177
James Reeves <jreeves@weavejester.com>
2012-08-27 13:06:24 +0100

    ragtime.sql.files loads migrations from SQL files
    Adapted from technomancy's pull request #1.

--- ragtime.sql.files/src/ragtime/sql/files.clj
+++ ragtime.sql.files/src/ragtime/sql/files.clj
@@ -1,6 +1,38 @@
-(ns ragtime.sql.files)
+(ns ragtime.sql.files
+  (:require [clojure.java.io :as io]
+            [clojure.java.jdbc :as sql]))
+
+(def ^:private migration-pattern
+  #"(.*)\.(up|down)\.sql$")
+
+(defn- migration? [file]
+  (re-find migration-pattern (.getName (io/file file))))
+
+(defn- migration-id [file]
+  (second (re-find migration-pattern (.getName (io/file file)))))
+
+(defn- get-migration-files [dir]
+  (->> (.listFiles (io/file dir))
+       (filter migration?)
+       (sort)
+       (group-by migration-id)))
+
+(defn- run-sql-fn [file]
+  (fn [db]
+    (sql/with-connection db
+      (sql/transaction
+       (let [f (slurp file)]
+         (prn f)
+         (sql/do-commands f))))))
+
+(defn- make-migration [[id [down up]]]
+  {:id   id
+   :up   (run-sql-fn up)
+   :down (run-sql-fn down)})
+
+(def ^:private default-dir "migrations")
 
 (defn migrations
   "Return a list of migrations to apply."
-  []
-  [])
+  ([]    (migrations default-dir))
+  ([dir] (map make-migration (get-migration-files dir))))
--- ragtime.sql.files/test/ragtime/sql/test/files.clj
+++ ragtime.sql.files/test/ragtime/sql/test/files.clj
@@ -1,6 +1,29 @@
 (ns ragtime.sql.test.files
+  (:require [clojure.java.jdbc :as sql]
+            [clojure.string :as str])
   (:use clojure.test
-        ragtime.sql.files))
+        ragtime.sql.files
+        ragtime.sql.database
+        ragtime.core))
+
+(def test-db
+  (connection "jdbc:h2:mem:test_db;DB_CLOSE_DELAY=-1"))
+
+(defn table-exists? [table-name]
+  (not-empty
+   (sql/with-query-results rs
+     ["select true from information_schema.tables where table_name = ?"
+      (str/upper-case table-name)]
+     (vec rs))))
 
 (deftest test-migrations
-  (is (= (migrations) [])))
+  (testing "no migration directory"
+    (is (= (migrations) [])))
+  (testing "custom migration directory"
+    (let [migs (migrations "test/migrations")]
+      (is (= (count migs) 1))
+      (is (= (:id (first migs)) "20111202110600-create-foo-table"))
+      (migrate-all test-db migs)
+      (sql/with-connection test-db
+        (is (table-exists? "ragtime_migrations"))
+        (is (table-exists? "foo"))))))
