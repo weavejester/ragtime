bc7a86ce37f9937de663636f32232d0d11f11485
James Reeves <jreeves@weavejester.com>
2012-09-03 21:51:11 +0100

    Updated lein-sub plugin version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[ragtime/ragtime.core "0.3.0-SNAPSHOT"]
                  [ragtime/ragtime.sql "0.3.0-SNAPSHOT"]
                  [ragtime/ragtime.sql.files "0.3.0-SNAPSHOT"]]
-  :plugins [[lein-sub "0.2.0"]]
+  :plugins [[lein-sub "0.2.1"]]
   :sub ["ragtime.core" "ragtime.sql" "ragtime.sql.files"])
