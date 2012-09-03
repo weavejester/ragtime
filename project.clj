9d5e0ecd00e55bf1ce32f95439195f79ba584045
James Reeves <jreeves@weavejester.com>
2012-09-03 21:59:10 +0100

    Added codox for API docs

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :dependencies [[ragtime/ragtime.core "0.3.0-SNAPSHOT"]
                  [ragtime/ragtime.sql "0.3.0-SNAPSHOT"]
                  [ragtime/ragtime.sql.files "0.3.0-SNAPSHOT"]]
-  :plugins [[lein-sub "0.2.1"]]
-  :sub ["ragtime.core" "ragtime.sql" "ragtime.sql.files"])
+  :plugins [[lein-sub "0.2.1"]
+            [codox "0.6.1"]]
+  :sub ["ragtime.core" "ragtime.sql" "ragtime.sql.files"]
+  :codox {:sources ["ragtime.core/src"
+                    "ragtime.sql/src"
+                    "ragtime.sql.files/src"]
+          :exclude [ragtime.sql.database]})
