f4490ce2eecddc76f57b204ead9082276c790c07
James Reeves <jreeves@weavejester.com>
2012-09-03 17:57:26 +0100

    Changed main package dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject ragtime "0.3.0-SNAPSHOT"
   :description "A database-independent migration library"
-  :dependencies [[ragtime/ragtime.lein "0.3.0-SNAPSHOT"]]
+  :dependencies [[ragtime/ragtime.core "0.3.0-SNAPSHOT"]
+                 [ragtime/ragtime.sql "0.3.0-SNAPSHOT"]
+                 [ragtime/ragtime.sql.files "0.3.0-SNAPSHOT"]]
   :plugins [[lein-sub "0.2.0"]]
   :sub ["ragtime.core" "ragtime.sql" "ragtime.sql.files"])
