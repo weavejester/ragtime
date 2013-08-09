8d2e63aec1556aa7062b798142b921a25035ab34
James Reeves <jreeves@weavejester.com>
2013-08-09 14:34:57 +0100

    Released 0.3.4

--- ragtime.core/project.clj
+++ ragtime.core/project.clj
@@ -1,4 +1,4 @@
-(defproject ragtime/ragtime.core "0.3.3"
+(defproject ragtime/ragtime.core "0.3.4"
   :description "A database-independent migration library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.2"]]
--- ragtime.lein/project.clj
+++ ragtime.lein/project.clj
@@ -1,4 +1,4 @@
-(defproject ragtime/ragtime.lein "0.3.3"
+(defproject ragtime/ragtime.lein "0.3.4"
   :description "A leiningen plugin for Ragtime."
   :dependencies [[leinjacker "0.4.1"]]
   :eval-in-leiningen true)
