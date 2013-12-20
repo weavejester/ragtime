b3f8989920d048ceceb43a0725e4d51613f2903d
Juha Syrjälä <juha.syrjala@gmail.com>
2013-12-20 19:44:38 +0200

    Update java.jdbc to 0.3.0.
    java.jdbc 0.3.0 contains breaking changes: DDL functionality has
    been moved to java-jdbc/dsl and API has changed. This commit
    modifies ragtime to work with java.jdbc 0.3.0 and to use DDL
    functionality from java-jdbc/dsl 0.1. Ragtime no longer works with
    java.jdbc 0.2.3 after this commit. Fixes issue #23.

--- ragtime.sql/project.clj
+++ ragtime.sql/project.clj
@@ -2,6 +2,7 @@
   :description "Ragtime migrations for SQL databases"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ragtime/ragtime.core "0.3.4"]
-                 [org.clojure/java.jdbc "0.2.3"]]
+                 [org.clojure/java.jdbc "0.3.0"]
+                 [java-jdbc/dsl "0.1.0"]]
   :profiles
   {:dev {:dependencies [[com.h2database/h2 "1.3.160"]]}})
