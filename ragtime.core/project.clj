6b91da3838ea7526b1752a438b80df62596e4b06
Sean Corfield <sean@corfield.org>
2014-01-09 19:42:54 -0800

    Exclude non-project compiled classes

--- ragtime.core/project.clj
+++ ragtime.core/project.clj
@@ -2,4 +2,6 @@
   :description "A database-independent migration library"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.2"]]
+  :aot [ragtime.main]
+  :clean-non-project-classes true
   :main ragtime.main)
