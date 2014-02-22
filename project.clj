148d569810fc71dfbaf71da986c39377d1456f03
James Reeves <jreeves@weavejester.com>
2014-02-22 17:59:37 +0000

    Fixed 'lein doc'

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
                  [ragtime/ragtime.sql "0.3.6"]
                  [ragtime/ragtime.sql.files "0.3.6"]]
   :plugins [[lein-sub "0.2.1"]
-            [codox "0.6.4"]]
+            [codox "0.6.7"]]
   :sub ["ragtime.core" "ragtime.sql" "ragtime.sql.files"]
   :codox {:sources ["ragtime.core/src"
                     "ragtime.sql/src"
                     "ragtime.sql.files/src"]
-          :exclude [ragtime.sql.database]})
+          :exclude [ragtime.sql.database ragtime.main]}
+  :profiles
+  {:dev {:dependencies [[codox/codox.core "0.6.7"]]}})
