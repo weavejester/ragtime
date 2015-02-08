f05127dd01a836d0de57231630fb7817f9f678a7
Martin Trojer <martin.trojer@gmail.com>
2015-02-08 19:07:31 +0000

    Made hardcoded migration table name overridable

--- ragtime.sql/src/ragtime/sql/database.clj
+++ ragtime.sql/src/ragtime/sql/database.clj
@@ -15,13 +15,13 @@
 
 (require-jdbc 'sql)
 
-(def ^:private migrations-table "ragtime_migrations")
+(def ^:dynamic *migrations-table* "ragtime_migrations")
 
 (defn ^:internal ensure-migrations-table-exists [db]
   ;; TODO: is there a portable way to detect table existence?
   (sql/with-connection db
     (try
-      (sql/create-table migrations-table
+      (sql/create-table *migrations-table*
                         [:id "varchar(255)"]
                         [:created_at "varchar(32)"])
       (catch Exception _))))
@@ -35,20 +35,20 @@
   (add-migration-id [db id]
     (sql/with-connection db
       (ensure-migrations-table-exists db)
-      (sql/insert-values migrations-table
+      (sql/insert-values *migrations-table*
                          [:id :created_at]
                          [(str id) (format-datetime (Date.))])))
-  
+
   (remove-migration-id [db id]
     (sql/with-connection db
       (ensure-migrations-table-exists db)
-      (sql/delete-rows migrations-table ["id = ?" id])))
+      (sql/delete-rows *migrations-table* ["id = ?" id])))
 
   (applied-migration-ids [db]
     (sql/with-connection db
       (ensure-migrations-table-exists db)
       (sql/with-query-results results
-        ["SELECT id FROM ragtime_migrations ORDER BY created_at"]
+        [(format "SELECT id FROM %s ORDER BY created_at" *migrations-table*)]
         (vec (map :id results))))))
 
 (defmethod connection "jdbc" [url]
