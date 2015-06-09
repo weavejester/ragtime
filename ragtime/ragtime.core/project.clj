758f4a9a74bc391fe4b71ff18a7ac3f57786b6df
James Reeves <jreeves@weavejester.com>
2015-06-09 16:29:06 +0100

    Remove ragtime.main namespace

--- ragtime/ragtime.core/project.clj
+++ ragtime/ragtime.core/project.clj
@@ -3,8 +3,4 @@
   :url "https://github.com/weavejester/ragtime"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.cli "0.2.2"]]
-  :aot [ragtime.main]
-  :clean-non-project-classes true
-  :main ragtime.main)
+  :dependencies [[org.clojure/clojure "1.3.0"]])
