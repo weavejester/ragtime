145261bc11a0c385abde0c3e85d0dfc897d3c760
James Reeves <jreeves@weavejester.com>
2015-06-09 16:28:07 +0100

    Add URL and license to ragtime.core project

--- ragtime/ragtime.core/project.clj
+++ ragtime/ragtime.core/project.clj
@@ -1,5 +1,8 @@
 (defproject ragtime/ragtime.core "0.3.9"
   :description "A database-independent migration library"
+  :url "https://github.com/weavejester/ragtime"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.2"]]
   :aot [ragtime.main]
