b8ce01d754a5d1cbcaebc6a560596e5582d5dc27
James Reeves <jreeves@weavejester.com>
2015-06-10 01:51:34 +0100

    Make migrations-table configurable

--- ragtime/ragtime.jdbc/src/ragtime/jdbc.clj
+++ ragtime/ragtime.jdbc/src/ragtime/jdbc.clj
@@ -6,39 +6,40 @@
            java.sql.SQLException
            java.text.SimpleDateFormat))
 
-(def ^:private migrations-table "ragtime_migrations")
-
-(def ^:private migrations-table-ddl
-  (sql/create-table-ddl migrations-table
+(defn- migrations-table-ddl [table-name]
+  (sql/create-table-ddl table-name
                         [:id "varchar(255)"]
                         [:created_at "varchar(32)"]))
 
-(defn ensure-migrations-table-exists [db-spec]
+(defn- ensure-migrations-table-exists [db-spec migrations-table]
   (try
-    (sql/execute! db-spec [migrations-table-ddl])
+    (sql/execute! db-spec [(migrations-table-ddl migrations-table)])
     (catch SQLException _)))
 
 (defn format-datetime [dt]
   (-> (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss.SSS")
       (.format dt)))
 
-(defrecord SqlDatabase [db-spec]
+(defrecord SqlDatabase [db-spec migrations-table]
   ragtime/Migratable
   (add-migration-id [_ id]
-    (ensure-migrations-table-exists db-spec)
+    (ensure-migrations-table-exists db-spec migrations-table)
     (sql/insert! db-spec migrations-table
                  {:id         (str id)
                   :created_at (format-datetime (Date.))}))
   
   (remove-migration-id [_ id]
-    (ensure-migrations-table-exists db-spec)
+    (ensure-migrations-table-exists db-spec migrations-table)
     (sql/delete! db-spec migrations-table ["id = ?" id]))
 
   (applied-migration-ids [_]
-    (ensure-migrations-table-exists db-spec)
+    (ensure-migrations-table-exists db-spec migrations-table)
     (sql/query db-spec
-      ["SELECT id FROM ragtime_migrations ORDER BY created_at"]
-      :row-fn :id)))
+               [(str "SELECT id FROM " migrations-table " ORDER BY created_at")]
+               :row-fn :id)))
 
-(defn sql-database [db-spec]
-  (->SqlDatabase db-spec))
+(defn sql-database
+  ([db-spec]
+   (sql-database db-spec {}))
+  ([db-spec {:keys [migrations-table] :or {migrations-table "ragtime_migrations"}}]
+   (->SqlDatabase db-spec migrations-table)))
--- ragtime/ragtime.jdbc/test/ragtime/jdbc_test.clj
+++ ragtime/ragtime.jdbc/test/ragtime/jdbc_test.clj
@@ -1,15 +1,21 @@
 (ns ragtime.jdbc-test
   (:require [clojure.test :refer :all]
             [ragtime.jdbc :refer :all]
-            [ragtime.core :as ragtime]))
-
-(def test-db
-  (sql-database {:connection-uri "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"}))
+            [ragtime.core :as ragtime]
+            [clojure.java.jdbc :as sql]))
 
 (deftest test-add-migrations
-  (ragtime/add-migration-id test-db "12")
-  (ragtime/add-migration-id test-db "13")
-  (ragtime/add-migration-id test-db "20")
-  (is (= ["12" "13" "20"] (ragtime/applied-migration-ids test-db)))
-  (ragtime/remove-migration-id test-db "13")
-  (is (= ["12" "20"] (ragtime/applied-migration-ids test-db))))
+  (let [db (sql-database {:connection-uri "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"})]
+    (ragtime/add-migration-id db "12")
+    (ragtime/add-migration-id db "13")
+    (ragtime/add-migration-id db "20")
+    (is (= ["12" "13" "20"] (ragtime/applied-migration-ids db)))
+    (ragtime/remove-migration-id db "13")
+    (is (= ["12" "20"] (ragtime/applied-migration-ids db)))))
+
+(deftest test-migrations-table
+  (let [db (sql-database {:connection-uri "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"}
+                         {:migrations-table "migrations"})]
+    (ragtime/add-migration-id db "12")
+    (is (= ["12"]
+           (sql/query (:db-spec db) ["SELECT * FROM migrations"] :row-fn :id)))))
