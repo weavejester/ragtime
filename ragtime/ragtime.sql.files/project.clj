bfaab521247821960f50b611dd99f92eabbbfd31
James Reeves <jreeves@weavejester.com>
2015-06-10 02:16:46 +0100

    Delete ragtime.sql.files project
    Functionality will be incorporated into ragtime.jdbc

--- ragtime/ragtime.sql.files/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject ragtime/ragtime.sql.files "0.3.9"
-  :description "Ragtime adapter that reads migrations from SQL files."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ragtime/ragtime.sql "0.3.9"]
-                 [org.clojure/java.jdbc "0.2.3"]]
-  :profiles
-  {:dev {:dependencies [[com.h2database/h2 "1.3.160"]]}
-   :java-jdbc-0.3.x [:dev {:dependencies [[org.clojure/java.jdbc "0.3.2"]]}]})
