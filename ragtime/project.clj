3dd046a445457bed9aab881855e587bdeedb7cfc
James Reeves <jreeves@weavejester.com>
2015-06-10 02:19:24 +0100

    Update the main Ragtime project file

--- ragtime/project.clj
+++ ragtime/project.clj
@@ -1,12 +1,9 @@
-(defproject ragtime "0.3.9"
+(defproject ragtime "0.4.0-SNAPSHOT"
   :description "A database-independent migration library"
-  :dependencies [[ragtime/ragtime.core "0.3.9"]
-                 [ragtime/ragtime.jdbc "0.3.9"]
-                 [ragtime/ragtime.sql.files "0.3.9"]]
+  :dependencies [[ragtime/ragtime.core "0.4.0-SNAPSHOT"]
+                 [ragtime/ragtime.jdbc "0.4.0-SNAPSHOT"]]
   :plugins [[lein-sub "0.2.1"]
             [codox "0.8.10"]]
-  :sub ["ragtime.core" "ragtime.jdbc" "ragtime.sql.files"]
+  :sub ["ragtime.core" "ragtime.jdbc"]
   :codox {:sources ["ragtime.core/src"
-                    "ragtime.jdbc/src"
-                    "ragtime.sql.files/src"]
-          :exclude [ragtime.jdbc ragtime.main]})
+                    "ragtime.jdbc/src"]})
