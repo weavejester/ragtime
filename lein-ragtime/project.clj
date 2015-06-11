60a972c14595a93a2df12a8db9d263a87c34cce6
James Reeves <jreeves@weavejester.com>
2015-06-11 02:45:14 +0100

    Rename ragtime.lein to lein-ragtime

--- lein-ragtime/project.clj
+++ lein-ragtime/project.clj
@@ -1,4 +1,4 @@
-(defproject ragtime/ragtime.lein "0.3.9"
+(defproject lein-ragtime "0.4.0-SNAPSHOT"
   :description "A leiningen plugin for Ragtime."
   :dependencies [[leinjacker "0.4.1"]]
   :eval-in-leiningen true)
