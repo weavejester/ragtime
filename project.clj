1cd92ee7b8ca3c0c652a3ac2f25cccf05882d14d
James Reeves <jreeves@weavejester.com>
2015-06-24 23:29:56 +0200

    Update Codox plugin to 0.8.12

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[ragtime/ragtime.core "0.4.0-SNAPSHOT"]
                  [ragtime/ragtime.jdbc "0.4.0-SNAPSHOT"]]
   :plugins [[lein-sub "0.3.0"]
-            [codox "0.8.11"]]
+            [codox "0.8.12"]]
   :sub ["ragtime.core" "ragtime.jdbc"]
   :codox {:sources ["ragtime.core/src"
                     "ragtime.jdbc/src"]})
