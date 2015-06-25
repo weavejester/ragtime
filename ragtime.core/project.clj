df0e2d6820bfa5de0ed9973ca5b14b466533ddcb
James Reeves <jreeves@weavejester.com>
2015-06-25 11:52:55 +0200

    Add :scm key to subprojects

--- ragtime.core/project.clj
+++ ragtime.core/project.clj
@@ -1,6 +1,7 @@
 (defproject ragtime/ragtime.core "0.4.0-SNAPSHOT"
   :description "A database-independent migration library"
   :url "https://github.com/weavejester/ragtime"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
--- ragtime.jdbc/project.clj
+++ ragtime.jdbc/project.clj
@@ -1,6 +1,7 @@
 (defproject ragtime/ragtime.jdbc "0.4.0-SNAPSHOT"
   :description "Ragtime migrations for JDBC"
   :url "https://github.com/weavejester/ragtime"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
