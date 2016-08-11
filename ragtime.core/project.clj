4d2a8e85089f6510a8fac61a9c511b04fb8837c0
James Reeves <jreeves@weavejester.com>
2016-08-11 22:28:33 +0100

    Release 0.6.2

--- ragtime.core/project.clj
+++ ragtime.core/project.clj
@@ -1,4 +1,4 @@
-(defproject ragtime/ragtime.core "0.6.1"
+(defproject ragtime/ragtime.core "0.6.2"
   :description "A database-independent migration library"
   :url "https://github.com/weavejester/ragtime"
   :scm {:dir ".."}
