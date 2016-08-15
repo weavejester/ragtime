7ca00d673d5a5baebecc827ba767f58d87d466ad
James Reeves <jreeves@weavejester.com>
2016-08-15 22:32:26 +0100

    Release 0.6.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject ragtime "0.6.2"
+(defproject ragtime "0.6.3"
   :description "A database-independent migration library"
   :url "https://github.com/weavejester/ragtime"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ragtime/ragtime.core "0.6.2"]
-                 [ragtime/ragtime.jdbc "0.6.2"]]
+  :dependencies [[ragtime/ragtime.core "0.6.3"]
+                 [ragtime/ragtime.jdbc "0.6.3"]]
   :plugins [[lein-sub "0.3.0"]
             [lein-codox "0.9.5"]]
   :sub ["ragtime.core" "ragtime.jdbc"]
