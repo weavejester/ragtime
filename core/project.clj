d2cad7176fba4a21f6bb54339cad0599d5723fd6
James Reeves <jreeves@weavejester.com>
2024-11-06 19:25:13 +0000

    Update minimum Clojure version to 1.10

--- core/project.clj
+++ core/project.clj
@@ -4,4 +4,4 @@
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]])
--- jdbc/project.clj
+++ jdbc/project.clj
@@ -4,7 +4,7 @@
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/java.jdbc "0.7.12"]
                  [dev.weavejester/ragtime.core "0.9.5"]
                  [resauce "0.2.0"]]
